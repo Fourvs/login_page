446d7321acd3c9c07ed362d50057f5b639b39505
Charvi Sharma <tech.charvi@gmail.com>
2026-03-25 14:48:13 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.html",         
+    "./src/**/*.{html,js}"
+  ],
+
   theme: {
     extend: {},
   },
